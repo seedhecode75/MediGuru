344738c808b4c78cabdd5bfe15c1c8cb63f44213
Sairam <s.boyidi@iitg.ac.in>
2025-06-13 16:02:01 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,13 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
